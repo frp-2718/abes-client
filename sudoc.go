b5925729b35eb1ac0f794590a0490d4a99f87431
frp <_frp@outlook.com>
2022-04-20 11:00:56 +0200

    feat: add a function to check ppn and a setClient method

--- sudoc.go
+++ sudoc.go
@@ -2,6 +2,7 @@ package sudoc
 
 import (
 	"net/http"
+	"regexp"
 	"time"
 )
 
@@ -19,4 +20,17 @@ func New() *Sudoc {
 	}
 	sudoc.Bibs = BibService{client: sudoc}
 	return sudoc
+}
+
+// SetClient allow user to provide a custom HTTP client.
+func (s *Sudoc) SetHTTPClient(client *http.Client) {
+	if client != nil {
+		s.client = client
+	}
+}
+
+// IsValidPPN checks if the given PPN is well formed.
+func IsValidPPN(ppn string) bool {
+	matched, _ := regexp.Match(`^[0-9]{8}?([0-9]{1}?|[xX])$`, []byte(ppn))
+	return matched
 }
--- sudoc_test.go
+++ sudoc_test.go
@@ -1,8 +1,10 @@
 package sudoc
 
 import (
+	"net/http"
 	"reflect"
 	"testing"
+	"time"
 )
 
 // Ad-hoc experimentation.
@@ -18,5 +20,38 @@ func TestSudoc(t *testing.T) {
 	if sudoc != nil {
 		assertNotNil(sudoc.client, t, ".client")
 		assertNotNil(sudoc.Bibs, t, ".Bibs")
+	}
+	myClient := &http.Client{
+		Timeout: time.Second * 5,
+	}
+	sudoc.SetHTTPClient(myClient)
+	assertNotNil(sudoc.client, t, ".client")
+	if sudoc.client != myClient {
+		t.Errorf("expected 'myClient' HTTP client, found %v", sudoc.client)
+	}
+	sudoc.SetHTTPClient(nil)
+	assertNotNil(sudoc.client, t, ".client")
+}
+
+func TestIsValidPPN(t *testing.T) {
+	var tests = []struct {
+		ppn  string
+		want bool
+	}{
+		{"123456789", true},
+		{"90873728x", true},
+		{"73826351X", true},
+		{"000000000", true},
+		{"", false},
+		{"1234567890", false},
+		{"a11222333", false},
+		{"11122233%", false},
+		{"         ", false},
+		{"32O25O11O", false},
+	}
+	for _, test := range tests {
+		if got := IsValidPPN(test.ppn); got != test.want {
+			t.Errorf("%s returned %v ; want %v", test.ppn, got, test.want)
+		}
 	}
 }
